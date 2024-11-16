a44eda2798c39b4dfb6aeb9c069f6b5460893917
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-11-15 17:19:34 -0800

    a5 - Lab5 - PathParameters.js and QueryParameters.js

--- App.js
+++ App.js
@@ -6,4 +6,4 @@ const app = express();
 HelloRoutes(app);
 Lab5(app);
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
--- Lab5/index.js
+++ Lab5/index.js
@@ -1,5 +1,9 @@
+import PathParameters from "./PathParameters.js";
+import QueryParameters from "./QueryParameters.js";
 export default function Lab5(app) {     // accept app reference to express module
     app.get("/lab5/welcome", (req, res) => {
         res.send("Welcome to Lab 5");
     });
+    PathParameters(app);
+    QueryParameters(app);
 };
