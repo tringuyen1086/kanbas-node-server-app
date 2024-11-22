ae3d806dbceb9d117b0b8402879bc9b9f12e2dd0
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-11-22 01:59:39 -0800

    a5 - lab5 - refactoring

--- App.js
+++ App.js
@@ -20,11 +20,11 @@ app.use(
         saveUninitialized: false,
     };
     if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-        domain: process.env.NODE_SERVER_DOMAIN,
+        sessionOptions.proxy = true;
+        sessionOptions.cookie = {
+            sameSite: "none",
+            secure: true,
+            domain: process.env.NODE_SERVER_DOMAIN,
     };
     }
     app.use(session(sessionOptions));
