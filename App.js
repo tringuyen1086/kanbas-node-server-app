30697c7352fda26eaa897ec9ea5e83394d22678c
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-11-21 17:54:37 -0800

    a5 - Kanbas - Account - 4.2 Integrating the Account Screens with the Server with RESTful Web APIs

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import HelloRoutes from './Hello.js';
 import Lab5 from './Lab5/index.js';
+import UserRoutes from "./Kanbas/Users/routes.js"
 import cors from "cors";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-HelloRoutes(app);
+// Register routes
+UserRoutes(app);
+
 Lab5(app);
+HelloRoutes(app);
+
 
 app.listen(process.env.PORT || 4000);
